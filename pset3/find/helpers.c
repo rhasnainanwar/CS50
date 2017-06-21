b00e4e9e4849de7efc0f1cd94d34418fc489d207
Raja Hasnain Anwar <rajahasnain_570@yahoo.com>
2017-06-21 18:27:38 +0000

    find done

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -14,6 +14,23 @@
 bool search(int value, int values[], int n)
 {
     // TODO: implement a searching algorithm
+    if(n > 0){
+        int start = 0;
+        int end  = n - 1;
+        int middle = (start + end)/2;
+        
+        while(start <= end){
+            if(values[middle] == value)
+                return true;
+            else if(values[middle] < value)
+                start = middle + 1;
+            else
+                end = middle - 1;
+                
+            middle = (start + end)/2;
+        }
+    }
+    //default
     return false;
 }
 
@@ -23,5 +40,16 @@ bool search(int value, int values[], int n)
 void sort(int values[], int n)
 {
     // TODO: implement a sorting algorithm
+    int temp = 0; //temporary variable for swapping
+    
+    for(int i = 0; i < n - 1; i++){
+        for(int j = 0; j < n - i - 1; ++j)
+            if(values[j] > values[j+1]){
+                temp = values[j];
+                values[j] = values[j+1];
+                values[j+1] = temp;
+            }
+    }
+
     return;
 }
